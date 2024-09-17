8c39028edbd8ad3971bc204b5b757a938f2eeb2f
Jkanare <Jyoti.kanare1992@gmail.com>
2024-09-17 21:18:46 +0530

    Nodejs assignment
    Nodejs assignment

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # node-app-repo
 This is Node App Repo
+
+
+# Project build steps
+1. Clone the repository - <repo-url>
+2. npm install or npm update
+
+# Author
+Jyoti Kanare
+Mobile: 8600 2550 99
---
+++ index.js
@@ -0,0 +1,36 @@
+const os = require('os');
+// console.log(`==============Global Object===================================`);
+
+console.log('My first app');
+// console.log(`File Name: ${__filename}`);
+// console.log(`Directory Name: ${__dirname}`);
+// console.log(`Global: ${global}`);
+// console.log(`Console: ${console}`);
+// console.log(`Process: ${process}`);
+// console.log(`Module: ${module}`);
+
+
+
+
+
+// console.log("=============== Built in Module - OS =======================");
+// console.log(`Arch: ${os.arch()}`);
+// console.log("Cpus:", os.cpus());
+// console.log("Free memory:" ,os.freemem());
+// console.log("Total memory:" ,os.totalmem());
+// console.log("Hostname:", os.hostname());
+// console.log("Operating system platform:" ,os.platform());
+// console.log("Os release:", os.release());
+// console.log("Os name:", os.type());
+// console.log("Uptime:", os.uptime());
+// console.log("User Information:", os.userInfo());
+
+console.log(`==============Global Object===================================`);
+
+console.log('My first app');
+console.log("File Name:", __filename);
+console.log("Directory Name:", __dirname);
+console.log("Global:", global);
+console.log("Console:", console);
+console.log("Process:", process);
+console.log("Module:", module);
